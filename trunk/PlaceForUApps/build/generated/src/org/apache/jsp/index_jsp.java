d795cef8575cb99bc85e938e27e9bf7d112695d0
ika09083 <ika09083@f765aec5-a8db-a96c-3b68-c1b6ab096e82>
2011-11-01 01:59:48 +0000

    update baru

--- trunk/PlaceForUApps/build/generated/src/org/apache/jsp/index_jsp.java
+++ trunk/PlaceForUApps/build/generated/src/org/apache/jsp/index_jsp.java
@@ -9,21 +9,12 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
 
-  private static java.util.List _jspx_dependants;
+  private static java.util.Vector _jspx_dependants;
 
-  private javax.el.ExpressionFactory _el_expressionfactory;
-  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public Object getDependants() {
     return _jspx_dependants;
-  }
-
-  public void _jspInit() {
-    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
-    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
-  }
-
-  public void _jspDestroy() {
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -38,7 +29,6 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     JspWriter _jspx_out = null;
     PageContext _jspx_page_context = null;
 
-
     try {
       response.setContentType("text/html;charset=UTF-8");
       pageContext = _jspxFactory.getPageContext(this, request, response,
@@ -49,6 +39,7 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       session = pageContext.getSession();
       out = pageContext.getOut();
       _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
       out.write("\n");
       out.write("\n");
@@ -58,32 +49,161 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("\n");
       out.write("<html>\n");
       out.write("    <head>\n");
-      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
-      out.write("        <title>JSP Page</title>\n");
-      out.write("    </head>\n");
-      out.write("    <body>\n");
-      out.write("        <h2>Login Member</h2>\n");
-      out.write("        <form method='post' action='login'>\n");
-      out.write("        <fieldset>\n");
-      out.write("        Username:\n");
-      out.write("        <input type='text' name='uname' />\n");
-      out.write("        <br><br>\n");
-      out.write("        Password:\n");
-      out.write("        <input type='password' name='pass' />\n");
-      out.write("        <br><br>\n");
-      out.write("        <input type='submit' value='Login' />\n");
-      out.write("        </fieldset>\n");
-      out.write("        <a href=\"/PlaceForUApps/register.jsp\">Register</a>\n");
-      out.write("        </form>\n");
-      out.write("    </body>\n");
+      out.write("        <meta name=\"keywords\" content=\"\" />\n");
+      out.write("        <meta name=\"description\" content=\"\" />\n");
+      out.write("        <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n");
+      out.write("        <title>PlaceForU - Cara Cepat Sewa Tempat dengan Cepat</title>\n");
+      out.write("<link href=\"css/style.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\" />\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("<div id=\"wrapper\">\n");
+      out.write("    <div id=\"logo\">\n");
+      out.write("\t<h1>PlaceForU</h1>\n");
+      out.write("\t<p>Cara Cepat Sewa Tempat dengan Cepat</p>\n");
+      out.write("    </div>\n");
+      out.write("<div id=\"header\">\n");
+      out.write("\t<div id=\"menu\">\n");
+      out.write("\t\t<ul>\n");
+      out.write("\t\t\t<li class=\"current_page_item\"><a href=\"#\">Homepage</a></li>\n");
+      out.write("\t\t</ul>\n");
+      out.write("\t</div>\n");
+      out.write("\t<div id=\"gallery\"><img src=\"images/img03.jpg\" width=\"692\" height=\"340\" alt=\"\" /></div>\n");
+      out.write("</div>\n");
+      out.write("<div id=\"page\">\n");
+      out.write("\t<div id=\"page-bgtop\">\n");
+      out.write("\t\t<div id=\"content\">\n");
+      out.write("\t\t\t<div class=\"post\">\n");
+      out.write("\t\t\t\t<h2 class=\"title\"><a href=\"#\">Selamat Datang di PlaceForU </a></h2>\n");
+      out.write("\t\t\t\t<p class=\"meta\"><span class=\"date\">April 10, 2011</span><span class=\"posted\">Posted by <a href=\"#\">Administrator</a></span></p>\n");
+      out.write("\t\t\t\t<div style=\"clear: both;\">&nbsp;</div>\n");
+      out.write("\t\t\t\t<div class=\"entry\">\n");
+      out.write("\t\t\t\t\t<p>This is <strong>Gastropoda </strong>, a free, fully standards-compliant CSS template designed by FreeCssTemplates<a href=\"http://www.nodethirtythree.com/\"></a> for <a href=\"http://www.freecsstemplates.org/\">Free CSS Templates</a>.  Photo used in this template is from <a href=\"http://www.pdphoto.org\">PDPhoto.org</a>. This free template is released under a <a href=\"http://creativecommons.org/licenses/by/2.5/\">Creative Commons Attributions 2.5</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :)</p>\n");
+      out.write("\t\t\t\t\t<p class=\"links\"><a href=\"#\">Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"#\">Comments</a></p>\n");
+      out.write("\t\t\t\t</div>\n");
+      out.write("\t\t\t</div>\n");
+      out.write("\t\t\t<div class=\"post\">\n");
+      out.write("\t\t\t\t<h2 class=\"title\"><a href=\"#\">Lorem ipsum sed aliquam</a></h2>\n");
+      out.write("\t\t\t\t<p class=\"meta\"><span class=\"date\">April 10, 2011</span><span class=\"posted\">Posted by <a href=\"#\">Someone</a></span></p>\n");
+      out.write("\t\t\t\t<div style=\"clear: both;\">&nbsp;</div>\n");
+      out.write("\t\t\t\t<div class=\"entry\">\n");
+      out.write("\t\t\t\t\t<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus <a href=\"#\">dapibus semper urna</a>. Pellentesque  orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem.  Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>\n");
+      out.write("\t\t\t\t\t<p class=\"links\"><a href=\"#\">Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"#\">Comments</a></p>\n");
+      out.write("\t\t\t\t</div>\n");
+      out.write("\t\t\t</div>\n");
+      out.write("\t\t\t<div class=\"post\">\n");
+      out.write("\t\t\t\t<h2 class=\"title\"><a href=\"#\">Consecteteur hendrerit </a></h2>\n");
+      out.write("\t\t\t\t<p class=\"meta\"><span class=\"date\">April 10, 2011</span><span class=\"posted\">Posted by <a href=\"#\">Someone</a></span></p>\n");
+      out.write("\t\t\t\t<div style=\"clear: both;\">&nbsp;</div>\n");
+      out.write("\t\t\t\t<div class=\"entry\">\n");
+      out.write("\t\t\t\t\t<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus  consectetuer. Donec ipsum. Proin imperdiet est. Phasellus <a href=\"#\">dapibus semper urna</a>. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut  nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem.  Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>\n");
+      out.write("\t\t\t\t\t<p class=\"links\"><a href=\"#\">Read More</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"#\">Comments</a></p>\n");
+      out.write("\t\t\t\t</div>\n");
+      out.write("\t\t\t</div>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t\t<div id=\"sidebar\">\n");
+      out.write("\t\t\t<ul>\n");
+      out.write("\t\t\t\t<li>\n");
+      out.write("\t\t\t\t\t<div id=\"search\" >\n");
+      out.write("\t\t\t\t\t\t<form method=\"get\" action=\"#\">\n");
+      out.write("\t\t\t\t\t\t\t<div>\n");
+      out.write("\t\t\t\t\t\t\t\t<input type=\"text\" name=\"s\" id=\"search-text\" value=\"\" />\n");
+      out.write("\t\t\t\t\t\t\t\t<input type=\"submit\" id=\"search-submit\" value=\"GO\" />\n");
+      out.write("\t\t\t\t\t\t\t</div>\n");
+      out.write("\t\t\t\t\t\t</form>\n");
+      out.write("\t\t\t\t\t</div>\n");
+      out.write("\t\t\t\t\t<div style=\"clear: both;\">&nbsp;</div>\n");
+      out.write("\t\t\t\t</li>\n");
+      out.write("\t\t\t\t<li>\n");
+      out.write("\t\t\t\t\t<h2>Log In</h2>\n");
+      out.write("\t\t\t\t\t<form method='post' action='login'>\n");
+      out.write("                                            <fieldset>\n");
+      out.write("                                                <table>\n");
+      out.write("                                                    <tr>\n");
+      out.write("                                                        <td>Username :</td>  \n");
+      out.write("                                                    </tr>\n");
+      out.write("                                                    <tr>\n");
+      out.write("                                                        <td><input type='text' name='uname' /></td>\n");
+      out.write("                                                    </tr>\n");
+      out.write("                                                    <tr>\n");
+      out.write("                                                        <td>Password :</td>  \n");
+      out.write("                                                    </tr>\n");
+      out.write("                                                    <tr>\n");
+      out.write("                                                        <td><input type='password' name='pass' /></td>\n");
+      out.write("                                                    </tr>\n");
+      out.write("                                                    <tr>\n");
+      out.write("                                                        <td><input type='submit' value='Login' />\n");
+      out.write("                                                        <a href=\"/PlaceForUApps/register.jsp\">Register</a></td>         \n");
+      out.write("                                                    </tr>\n");
+      out.write("                                                </table>\n");
+      out.write("                                            </fieldset>\n");
+      out.write("                                            </form>\n");
+      out.write("\t\t\t\t</li>\n");
+      out.write("\t\t\t\t<li>\n");
+      out.write("\t\t\t\t\t<h2>Gedung Terbaru</h2>\n");
+      out.write("\t\t\t\t\t<ul>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Aliquam libero</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Consectetuer adipiscing elit</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Metus aliquam pellentesque</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Suspendisse iaculis mauris</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Urnanet non molestie semper</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Proin gravida orci porttitor</a></li>\n");
+      out.write("\t\t\t\t\t</ul>\n");
+      out.write("\t\t\t\t</li>\n");
+      out.write("\t\t\t\t<li>\n");
+      out.write("\t\t\t\t\t<h2>Blogroll</h2>\n");
+      out.write("\t\t\t\t\t<ul>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Aliquam libero</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Consectetuer adipiscing elit</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Metus aliquam pellentesque</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Suspendisse iaculis mauris</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Urnanet non molestie semper</a></li>\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Proin gravida orci porttitor</a></li>\n");
+      out.write("\t\t\t\t\t</ul>\n");
+      out.write("\t\t\t\t</li>\n");
+      out.write("\t\t\t</ul>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t</div>\n");
+      out.write("</div>\n");
+      out.write("<div id=\"footer-bg\">\n");
+      out.write("\t<div id=\"footer-content\">\n");
+      out.write("\t\t<div id=\"col1\">\n");
+      out.write("\t\t\t<ul class=\"style1\">\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Etiam malesuada rutrum enim</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Aenean elementum facilisis ligula</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Ut tincidunt elit vitae augue</a></li>\n");
+      out.write("\t\t\t</ul>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t\t<div id=\"col2\">\n");
+      out.write("\t\t\t<ul class=\"style1\">\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Etiam malesuada rutrum enim</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Aenean elementum facilisis ligula</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Ut tincidunt elit vitae augue</a></li>\n");
+      out.write("\t\t\t</ul>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t\t<div id=\"col3\">\n");
+      out.write("\t\t\t<ul class=\"style1\">\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Etiam malesuada rutrum enim</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Aenean elementum facilisis ligula</a></li>\n");
+      out.write("\t\t\t\t<li><a href=\"#\">Ut tincidunt elit vitae augue</a></li>\n");
+      out.write("\t\t\t</ul>\n");
+      out.write("\t\t</div>\n");
+      out.write("\t</div>\n");
+      out.write("</div>\n");
+      out.write("<div id=\"footer\">\n");
+      out.write("\t<p>Copyright (c) 2011 Sitename.com. All rights reserved. Design by <a href=\"http://www.freecsstemplates.org/\">CSS Templates</a>.</p>\n");
+      out.write("</div>\n");
+      out.write("</div>\n");
+      out.write("<!-- end #footer -->\n");
+      out.write("</body>\n");
       out.write("</html>\n");
       out.write("\n");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
         out = _jspx_out;
         if (out != null && out.getBufferSize() != 0)
-          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+          out.clearBuffer();
         if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
       }
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
